0a4ea61b04b7a9e83a0f9cda9679866a604b46a2
hongkim25 <for20170101@gmail.com>
2025-09-18 21:32:55 +0900

    commit

---
+++ git_practice/250918_js/03_operator.js
@@ -0,0 +1,8 @@
+console.log("1+1 =", 1 + 1);
+console.log("0" == false);
+console.log("0" == 0);
+console.log("0" == false);
+console.log("0" === false);
+
+console.log("" === "0");
+console.log("" == "0");
