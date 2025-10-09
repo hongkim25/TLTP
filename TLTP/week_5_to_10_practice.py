a76719ab507ba32708ef2f23f67a9269551de08f
hongkim25 <for20170101@gmail.com>
2025-10-09 14:05:24 +0900

    codewars

--- TLTP/week_5_to_10_practice.py
+++ TLTP/week_5_to_10_practice.py
@@ -2,6 +2,7 @@ print(12/(1+2)+2**2)
 
 # Day 70: Codewars
 # Sum of positive sums: You get an array of numbers, return the sum of all of the positives ones.
+# The first is mine, the second is the best practice.
 def positive_sum(arr):
     sum = 0
     for a in arr:    
@@ -11,3 +12,6 @@ def positive_sum(arr):
             a = a
         sum += a
     return sum
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
