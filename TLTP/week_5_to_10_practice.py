c16c12efcba3f284e545470eb64957b2c86bc271
hongkim25 <for20170101@gmail.com>
2025-10-09 14:03:15 +0900

    codwars

--- TLTP/week_5_to_10_practice.py
+++ TLTP/week_5_to_10_practice.py
@@ -1,1 +1,13 @@
 print(12/(1+2)+2**2)
+
+# Day 70
+# Codewars sum of positive sums
+def positive_sum(arr):
+    sum = 0
+    for a in arr:    
+        if a < 0:
+            a = 0
+        else:
+            a = a
+        sum += a
+    return sum
