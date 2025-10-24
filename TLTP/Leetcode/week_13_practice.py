5b5045a4769302f8224808772baa836b1f5233ea
hongkim25 <for20170101@gmail.com>
2025-10-24 22:45:52 +0900

    typo fix

--- TLTP/Leetcode/week_13_practice.py
+++ TLTP/Leetcode/week_13_practice.py
@@ -12,14 +12,14 @@ class Solution:
             curr = temp
         return prev
     
-# Rercursive: Time O(n) Space O(n)
+# Rercursive Time O(n) Space O(n)
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
 
         if not head:
             return None
         
-        newHead = head.next:
+        newHead = head
         if head.next:
             newHead = self.reverseList(head.next)
             head.next.next = head
