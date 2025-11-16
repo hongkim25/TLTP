a581e70c09131f341dffee5a3354070e86ffd261
hongkim25 <for20170101@gmail.com>
2025-11-16 21:45:34 +0900

    daily log update

--- TLTP/Leetcode/week_16_practice.py
+++ TLTP/Leetcode/week_16_practice.py
@@ -20,3 +20,28 @@ class Solution:
                 stack.append(c)
 
         return True if not stack else False
+    
+
+# Day 108, 16 NOV 2025
+# LeetCode 155: Design Min Stack
+# Stack: Time O(1) Space O(n)
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
