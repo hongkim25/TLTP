8eb628546cb2d57db002e046acbbe456e6d25091
hongkim25 <for20170101@gmail.com>
2025-12-05 20:46:12 +0900

    daily leetcode

--- TLTP/Leetcode/week_19_practice.py
+++ TLTP/Leetcode/week_19_practice.py
@@ -2,4 +2,15 @@
 
 # Day 127, 5 DEC 2025
 # LeetCode 62: Unique Paths
-# 
+# Dynamic Programming (2D DP): Time O(m * n) Space O(n)
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
