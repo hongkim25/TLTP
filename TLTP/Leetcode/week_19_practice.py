1c399036e79b1496575de6d841861142862845c7
hongkim25 <for20170101@gmail.com>
2025-12-11 15:07:11 +0900

    daily leetcode 2

--- TLTP/Leetcode/week_19_practice.py
+++ TLTP/Leetcode/week_19_practice.py
@@ -139,3 +139,32 @@ class Solution:
             if i1.end > i2.start:
                 return False
         return True
+    
+
+# LeetCode 253: Meeting Rooms II
+# Sorting + Two Pointers: Time O(n log n) Space O(n)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
