7b0ff918d2af7390526a6c3e508772b16f286371
hongkim25 <for20170101@gmail.com>
2025-12-13 15:49:53 +0900

    daily leetcode

--- TLTP/Leetcode/week_20_practice.py
+++ TLTP/Leetcode/week_20_practice.py
@@ -19,4 +19,28 @@ class Solution:
                 ]
         res.append(newInterval)
         return res
-    
+
+# Day 135, 13 DEC 2025
+# LeetCode 202: Happy Number
+# HashSet: Time O(log n) Space O(log n)    
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+            if n == 1:
+                return True
+        return False
+
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            output += digit
+            n = n // 10
+        return output
