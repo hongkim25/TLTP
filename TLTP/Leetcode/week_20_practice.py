af213e8f704929e9001b91368a63088846672d15
hongkim25 <for20170101@gmail.com>
2025-12-14 17:32:12 +0900

    daily leetcode

--- TLTP/Leetcode/week_20_practice.py
+++ TLTP/Leetcode/week_20_practice.py
@@ -51,3 +51,27 @@ class Solution:
 
 # while n means while n != 0
 
+# Day 135, 13 DEC 2025
+# LeetCode 66: Plus One
+# Time O(n) Space O(1)
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        one = 1
+        i = 0
+        digits.reverse()
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(one)
+                one = 0
+            i += 1
+
+        digits.reverse()
+        return digits
