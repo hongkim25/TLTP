b61a8a21db2018b8060f446b04c8b19d2df5909f
hongkim25 <for20170101@gmail.com>
2025-12-18 20:33:24 +0900

    daily leetcode

--- TLTP/Leetcode/week_20_practice.py
+++ TLTP/Leetcode/week_20_practice.py
@@ -142,7 +142,7 @@ class Solution:
     
 ### NEETCODE 150 ARRAYS & HASHING QUESTIONS ###
 
-# # Day 139, 17 DEC 2025
+# Day 139, 17 DEC 2025
 # LeetCode 49: Group Anagrams
 # Hash Table: Time O(m * n) Space O(m)
 
@@ -155,3 +155,25 @@ class Solution:
                 count[ord(c) - ord('a')] += 1
             res[tuple(count)].append(s)
         return list(res.values())
+    
+
+# Day 140, 18 DEC 2025
+# LeetCode 347: Top K Frequent Elements
+# Bucket Sort: Time O(n) Space O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
