d348da60bc5137a9ce9aea2d110b6ac1e544ee76
hongkim25 <for20170101@gmail.com>
2025-12-21 18:43:06 +0900

    daily leetcode

--- TLTP/Leetcode/week_21_practice.py
+++ TLTP/Leetcode/week_21_practice.py
@@ -43,3 +43,28 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+    
+# Day 143, 21 DEC 2025
+# LeetCode 36: Valid Sudoku
+# Hash Set: Time O(n^2) Space O(n^2)
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
