6029c661cf5244875740cfd0d5fcce169447bc16
hongkim25 <for20170101@gmail.com>
2025-12-24 20:15:46 +0900

    daily leetcode

--- TLTP/Leetcode/week_21_practice.py
+++ TLTP/Leetcode/week_21_practice.py
@@ -104,3 +104,26 @@ class Solution:
             else:
                 r -= 1
         return res
+    
+# Day 146, 24 DEC 2025
+# LeetCode 42: Trapping Rain Water
+# Two Pointers: Time O(n) Space O(1)
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
