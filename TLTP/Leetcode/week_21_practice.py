adb715cd5829f802925611b8f9d4904b250eabd6
hongkim25 <for20170101@gmail.com>
2025-12-25 21:29:59 +0900

    daily leetcode

--- TLTP/Leetcode/week_21_practice.py
+++ TLTP/Leetcode/week_21_practice.py
@@ -127,3 +127,26 @@ class Solution:
                 rightMax = max(rightMax, height[r])
                 res += rightMax - height[r]
         return res
+    
+# Day 147, 25 DEC 2025
+# LeetCode 424: Longest Repeating Character Replacement
+# Sliding Window: Time O(m * n) Space O(m)
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        charSet = set(s)
+
+        for c in charSet:
+            count = l = 0
+            for r in range(len(s)):
+                if s[r] == c:
+                    count += 1
+
+                while (r - l + 1) - count > k:
+                    if s[l] == c:
+                        count -= 1
+                    l += 1
+
+                res = max(res, r - l + 1)
+        return res
