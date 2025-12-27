f9487961fe040f744d7579c2e0e056c23af99146
hongkim25 <for20170101@gmail.com>
2025-12-27 13:18:57 +0900

    daily leetcode

--- TLTP/Leetcode/week_22_practice.py
+++ TLTP/Leetcode/week_22_practice.py
@@ -36,3 +36,26 @@ class Solution:
                 matches -= 1
             l += 1
         return matches == 26
+    
+
+# Day 149, 27 DEC 2025
+# LeetCode 150: Evaluate Reverse Polish Notation
+# Stack: Time O(n) Space O(n)
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
