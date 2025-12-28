762ab4b1ebaa7bbfe5385f0c675aabaca4c9c8a1
hongkim25 <for20170101@gmail.com>
2025-12-28 18:48:49 +0900

    daily leetcode

--- TLTP/Leetcode/week_22_practice.py
+++ TLTP/Leetcode/week_22_practice.py
@@ -59,3 +59,20 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
+    
+
+    # Day 150, 28 DEC 2025
+    # LeetCode 739: Daily Temperatures
+    # Monotonic Stack: Time O(n) Space O(n)
+
+    class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
