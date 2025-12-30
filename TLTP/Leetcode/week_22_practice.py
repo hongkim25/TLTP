06ccf5f93cb7c274eb3a8ef303d9df1d9cfa357c
hongkim25 <for20170101@gmail.com>
2025-12-30 16:14:54 +0900

    daily leetcode

--- TLTP/Leetcode/week_22_practice.py
+++ TLTP/Leetcode/week_22_practice.py
@@ -61,18 +61,59 @@ class Solution:
         return stack[0]
     
 
-    # Day 150, 28 DEC 2025
-    # LeetCode 739: Daily Temperatures
-    # Monotonic Stack: Time O(n) Space O(n)
+# Day 150, 28 DEC 2025
+# LeetCode 739: Daily Temperatures
+# Monotonic Stack: Time O(n) Space O(n)
 
-    class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
-        stack = []  # pair: [temp, index]
+class Solution:
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    res = [0] * len(temperatures)
+    stack = []  # pair: [temp, index]
 
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
+    for i, t in enumerate(temperatures):
+        while stack and t > stack[-1][0]:
                 stackT, stackInd = stack.pop()
                 res[stackInd] = i - stackInd
-            stack.append((t, i))
+        stack.append((t, i))
+    return res
+    
+# The order of variables in for i, t is simply how enumerate() works, 
+# but the stack always stores the pair as (temperature, index).
+
+# Day 151, 29 DEC 2025
+# LeetCode 853: Car Fleet
+# Stack: Time O(n log n) Space O(n)
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+
+# Day 152, 30 DEC 2025
+# LeetCode 875: Koko Eating Bananas
+# Binary Search: Time O(n log m) Space O(1)
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
         return res
