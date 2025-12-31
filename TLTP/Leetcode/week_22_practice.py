b90dab3a5f48e15d2a86e75a3b99b2055009c49f
hongkim25 <for20170101@gmail.com>
2025-12-31 18:36:04 +0900

    daily leetcode

--- TLTP/Leetcode/week_22_practice.py
+++ TLTP/Leetcode/week_22_practice.py
@@ -117,3 +117,31 @@ class Solution:
             else:
                 l = k + 1
         return res
+    
+
+# Day 153, 31 DEC 2025
+# LeetCode 33: Search in Rotated Sorted Array
+# Binary Search (One Pass): Time O(log n) Space O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+
