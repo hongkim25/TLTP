d98e1fb62c1a580b7b013d855c35ad2a5cb16131
hongkim25 <for20170101@gmail.com>
2026-01-03 10:27:45 +0900

    daily leetcode

--- TLTP/Leetcode/week_23_practice.py
+++ TLTP/Leetcode/week_23_practice.py
@@ -43,3 +43,39 @@ class Solution {
         }
     }
 }
+
+# Day 156, 03 JAN 2026
+# LeetCode 19: Remove Nth Node From End of List
+# Time O(n) Space O(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy;
+        ListNode right = head;
+
+        while (n > 0) {
+            right = right.next;
+            n--;
+        }
+
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
