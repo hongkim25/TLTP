aa3694e18b538b8dbb07edeefdc5a41e9672cd3f
hongkim25 <for20170101@gmail.com>
2026-01-06 19:00:18 +0900

    daily leetcode

--- TLTP/Leetcode/week_23_practice.py
+++ TLTP/Leetcode/week_23_practice.py
@@ -147,3 +147,26 @@ class Solution:
             l2 = l2.next if l2 else None
 
         return dummy.next
+    
+# Day 159, 06 JAN 2026
+# LeetCode 110: Balanced Binary Tree
+# DFS: Time O(n) Space O(h)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
