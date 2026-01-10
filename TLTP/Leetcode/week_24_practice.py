fc25962101b8019c325bb9d696e04ebc1e0911b9
hongkim25 <for20170101@gmail.com>
2026-01-10 19:09:52 +0900

    daily leetcode

--- TLTP/Leetcode/week_24_practice.py
+++ TLTP/Leetcode/week_24_practice.py
@@ -29,3 +29,34 @@ class Solution:
                 res.append(level)
 
         return res
+
+
+# Day 163, 10 JAN 2026
+# LeetCode 199: Binary Tree Right Side View
+# BFS: Time O(n) Space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
