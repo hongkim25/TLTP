7f91e8b7f09aa711411c056d694f2d60d4ab8e00
hongkim25 <for20170101@gmail.com>
2026-01-11 20:51:52 +0900

    daily leetcode

--- TLTP/Leetcode/week_24_practice.py
+++ TLTP/Leetcode/week_24_practice.py
@@ -60,3 +60,29 @@ class Solution:
             if rightSide:
                 res.append(rightSide.val)
         return res
+
+# Day 164, 11 JAN 2026
+# LeetCode 1448: Count Good Nodes in Binary Tree
+# DFS: Time O(n) Space O(n)    
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
