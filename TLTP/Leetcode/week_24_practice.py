bda95418d10bf1b0a365c360878f62fb6fa239a4
hongkim25 <for20170101@gmail.com>
2026-01-14 17:45:33 +0900

    daily leetcode

--- TLTP/Leetcode/week_24_practice.py
+++ TLTP/Leetcode/week_24_practice.py
@@ -140,3 +140,24 @@ class Solution:
                 return curr.val
             curr = curr.right
 
+
+# Day 167, 14 JAN 2026
+# LeetCode 973: K Closest Points to Origin
+# Min-Heap: Time O(n + k * log n) Space O(n)
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append([dist, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+
+        return res
+
