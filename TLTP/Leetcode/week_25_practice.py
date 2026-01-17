d7c4eb42749090ade6d3c99e2a4c02aa4291f2e4
hongkim25 <for20170101@gmail.com>
2026-01-17 21:32:58 +0900

    daily leetcode

--- TLTP/Leetcode/week_25_practice.py
+++ TLTP/Leetcode/week_25_practice.py
@@ -32,3 +32,34 @@ class Solution:
                         fresh -= 1
             time += 1
         return time if fresh == 0 else -1
+    
+
+# Day 170, 17 JAN 2026
+# LeetCode 5: Longest Palindromic Substring
+# Expand Around Center: Time O(n^2) Space O(1)
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resIdx = 0
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return s[resIdx : resIdx + resLen]
