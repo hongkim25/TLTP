9b53165c9b8578169f83a504033b2fda9f3103cc
hongkim25 <for20170101@gmail.com>
2026-01-18 23:32:40 +0900

    daily leetcode

--- TLTP/Leetcode/week_25_practice.py
+++ TLTP/Leetcode/week_25_practice.py
@@ -63,3 +63,26 @@ class Solution:
                 r += 1
 
         return s[resIdx : resIdx + resLen]
+
+
+# Day 171, 18 JAN 2026
+# LeetCode 647: Palindromic Substrings
+# Two Pointers (Expand Around Center): Time O(n^2) Space O(1)
+
+class Solution:
+
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            res += self.countPali(s, i, i)
+            res += self.countPali(s, i, i + 1)
+        return res
+
+    def countPali(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
