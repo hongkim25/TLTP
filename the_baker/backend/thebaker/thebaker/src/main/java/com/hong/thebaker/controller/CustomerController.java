9a655cea654c9beff9fa84bae51c38d24f35a018
hongkim25 <for20170101@gmail.com>
2026-01-18 19:13:11 +0900

    minor fix

--- the_baker/backend/thebaker/thebaker/src/main/java/com/hong/thebaker/controller/CustomerController.java
+++ the_baker/backend/thebaker/thebaker/src/main/java/com/hong/thebaker/controller/CustomerController.java
@@ -1,4 +1,32 @@
 package com.hong.thebaker.controller;
 
+import com.hong.thebaker.entity.Customer;
+import com.hong.thebaker.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // GET /api/customers/{phone}
+    // Used by index.html to show points to the customer
+    @GetMapping("/{phone}")
+    public ResponseEntity<?> getCustomerPoints(@PathVariable String phone) {
+        Optional<Customer> customer = customerRepository.findByPhone(phone);
+
+        if (customer.isPresent()) {
+            // Return just the points, or the whole object
+            return ResponseEntity.ok(customer.get());
+        } else {
+            // If new customer, return 0 points (don't error out)
+            return ResponseEntity.ok().body("{\"points\": 0}");
+        }
+    }
 }
